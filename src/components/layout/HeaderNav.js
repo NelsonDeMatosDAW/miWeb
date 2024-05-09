4a1e7ff12560732926cf0bac5a251a90509c9b8d
nelsonDeMatosDaw <nelsondematos16@gmail.com>
2024-05-09 23:31:54 +0200

    Web con app PWA para android, falta establecer para ios

--- src/components/layout/HeaderNav.js
+++ src/components/layout/HeaderNav.js
@@ -1,9 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import "./css/header.css"
 
-export const HeaderNav = () => {
+export const HeaderNav = (props) => {
+
+  const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
 
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -19,37 +53,32 @@ export const HeaderNav = () => {
       <div className='logo'>
         <h3>NelsonDeMatos<b>Designer</b></h3>
       </div>
-        
+      <div className='divDescarga'>
+        {isReadyForInstall && (
+          <button onClick={downloadApp}><img src={`${process.env.PUBLIC_URL}/img/descargar.png`} alt='descargar' id='imgDescargar'/> APP</button>
+        )}
+        {/* <img src={`${process.env.PUBLIC_URL}/img/descargar.png`} alt='descargar' id='imgDescargar'/> */}
+      </div>
+      
+      <div className='divNav'>
         <nav>
-            <button className='menu-button' onClick={toggleMenu}>
+          <button className='menu-button' onClick={toggleMenu}>
               ☰
-            </button>
-            <ul className={menuOpen ? 'open' : 'close'}>
-                <li>
-                  <NavLink to="/inicio" activeclass="active" onClick={toggleMenu}>Inicio</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/portafolio" activeclass="active" onClick={toggleMenu}>Portafolio</NavLink>
-                </li>
-                {/*
-                <li>
-                  <NavLink to="/servicios" activeclass="active" onClick={toggleMenu}>Servicios</NavLink>
-                </li>
-                */}
-                <li>
-                  <NavLink to="/curriculum" activeclass="active" onClick={toggleMenu}>Curriculum</NavLink>
-                </li>
-                {/*
-                <li>
-                  <NavLink to="/contacto" activeclass="active" onClick={toggleMenu}>Contacto</NavLink>
-                </li>
-                */}
-
-            </ul>
+          </button>
+          <ul className={menuOpen ? 'open' : 'close'}>
+            <li>
+              <NavLink to="/inicio" activeclass="active" onClick={toggleMenu}>Inicio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/portafolio" activeclass="active" onClick={toggleMenu}>Portafolio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/curriculum" activeclass="active" onClick={toggleMenu}>Curriculum</NavLink>
+            </li>
+          </ul>
         </nav>
-
+      </div>
     </header>
-    
     
   )
 }
